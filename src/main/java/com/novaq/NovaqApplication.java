f90b9ffa9c80982087e6b3f254d382ac455afcd8
kayk <kayksantana133@gmail.com>
2026-06-08 18:11:49 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/novaq/NovaqApplication.java
@@ -0,0 +1,13 @@
+package com.novaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novaq/NovaqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novaq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
